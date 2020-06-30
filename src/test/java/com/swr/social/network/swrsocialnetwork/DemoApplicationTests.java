1dcd28f4cdad83db210df158acd4f4eff9152a68
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-06-29 23:29:53 -0300

    Project creation

---
+++ src/test/java/com/swr/social/network/swrsocialnetwork/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swr.social.network.swrsocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
