c688b16d31e6bb9c84047394b0cebee1321b0e1c
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-07-01 23:38:43 -0300

    Adding PostMapping request of the rebel's

--- src/main/java/com/swr/social/network/swrsocialnetwork/rebels/controllers/RebelControllerV1.java
+++ src/main/java/com/swr/social/network/swrsocialnetwork/rebels/controllers/RebelControllerV1.java
@@ -2,6 +2,7 @@ package com.swr.social.network.swrsocialnetwork.rebels.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
 
 import com.swr.social.network.swrsocialnetwork.rebels.exceptions.RebelNotFoundException;
 import com.swr.social.network.swrsocialnetwork.rebels.models.Rebel;
@@ -12,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +40,11 @@ public class RebelControllerV1 {
 
         return new ResponseEntity<Rebel>(rebel, HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity<Rebel> addRebel(@Valid @RequestBody Rebel rebel) {
+        return new ResponseEntity<Rebel>(rebelService.save(rebel), HttpStatus.CREATED);
+    }
+
 
 }
