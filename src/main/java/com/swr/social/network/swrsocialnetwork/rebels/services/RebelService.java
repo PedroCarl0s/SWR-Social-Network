ed99b9a9b9470792f4c14b6be4f460c03078b915
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-07-01 23:32:55 -0300

    Adding find and save of rebel's

--- src/main/java/com/swr/social/network/swrsocialnetwork/rebels/services/RebelService.java
+++ src/main/java/com/swr/social/network/swrsocialnetwork/rebels/services/RebelService.java
@@ -1,7 +1,10 @@
 package com.swr.social.network.swrsocialnetwork.rebels.services;
 
+import java.util.List;
+import java.util.Optional;
 
 import com.swr.social.network.swrsocialnetwork.rebels.repositories.RebelRepository;
+import com.swr.social.network.swrsocialnetwork.rebels.models.Rebel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +14,18 @@ public class RebelService {
 
     @Autowired
     private RebelRepository rebelDAO;
+
+
+    public List<Rebel> findAll() {
+        return rebelDAO.findAll();
+    }
+
+    public Optional<Rebel> findById(Long id) {
+        return rebelDAO.findById(id);
+    }
+
+    public Rebel save(Rebel rebel) {
+        return rebelDAO.save(rebel);
+    }
 
 }
