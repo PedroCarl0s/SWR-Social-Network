9ba695e48d0a39a457f9c22d678646c1be4cfeb6
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-07-02 20:57:04 -0300

    Adding documentation in the model of location

--- src/main/java/com/swr/social/network/swrsocialnetwork/rebels/models/Location.java
+++ src/main/java/com/swr/social/network/swrsocialnetwork/rebels/models/Location.java
@@ -5,19 +5,25 @@ import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @Embeddable
 public class Location {
 
+    @ApiModelProperty(notes = "Latitude of the rebel's location", name = "latitude", required = true, example = "123.99789")
     @NotNull
     private double latitude;
 
+    @ApiModelProperty(notes = "Longitude of the rebel's location", name = "longitude", required = true, example = "456.12345")
     @NotNull
     private double longitude;
 
+    @ApiModelProperty(notes = "Galaxy name of the rebel's", name = "galaxyName", required = true, example = "Milky Way")
     @NotNull
     @Size(max = 80)
     private String galaxyName;
 
+    @ApiModelProperty(notes = "Base name of the rebel's", name = "baseName", required = true, example = "Way")
     @NotNull
     @Size(max = 80)
     private String baseName;
