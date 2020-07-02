81491164508d2565987714ea9fc9146e6f81c45a
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-07-01 23:34:54 -0300

    Adding RebelNotFoundException

---
+++ src/main/java/com/swr/social/network/swrsocialnetwork/rebels/exceptions/RebelNotFoundException.java
@@ -0,0 +1,12 @@
+package com.swr.social.network.swrsocialnetwork.rebels.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RebelNotFoundException extends RuntimeException {
+
+    public RebelNotFoundException() {
+        super("Rebel not found!");
+    }
+}
